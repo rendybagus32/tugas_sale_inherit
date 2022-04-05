be0f7dc7dd62be2aeb49fd7164193c9f7a233af1
rendybagus <rendycalai@gmail.com>
2022-04-05 08:55:51 +0700

    commit 2

--- tugas_inherit__sale/models/inherit_sale.py
+++ tugas_inherit__sale/models/inherit_sale.py
@@ -4,8 +4,16 @@ from odoo import models
 class Inherit_Sale(models.Model):
     _inherit = 'sale.order'
 
-    # button_create_mo = fields.Integer(string='Cost Production', required=True)
-
-    def button_create_mo(self):
-        # self.state = 'done'
-        self.action_confirm()
+    def create_mo(self):
+        for record in self:
+            bom = record.order_line.product_id.bom_ids.id
+            if bom:
+                self.env['mrp.production'].create(
+                    {
+                    'product_id': record.order_line.product_id.id,
+                    'product_qty': record.order_line.product_uom_qty,
+                    'product_uom_id': record.order_line.product_uom.id,
+                    'bom_id': record.order_line.product_id.bom_ids.id,
+                    })
+            else:
+                pass
